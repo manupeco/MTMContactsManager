48c0c4648179032548027d2614de7ab717b483af
manupeco <emanuele.pecorari@gmail.com>
2014-11-18 22:26:20 +0100

    Removed Example folder

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  MTMContactsManagerTests.m
-//  MTMContactsManagerTests
-//
-//  Created by manupeco on 11/18/2014.
-//  Copyright (c) 2014 manupeco. All rights reserved.
-//
-
-${TEST_EXAMPLE}
